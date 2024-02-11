fc4a1f05e97ab9d3ddf46e052367cc929914f0c7
Kewen silva <kewensilva58@gmail.com>
2024-02-11 18:16:50 -0300

    [fix: ciração de layouts e componentes e rota home e cadastro]

---
+++ frontend/components/inputField.py
@@ -0,0 +1,20 @@
+import flet as ft
+from flet import TextField, Container, Row,border, TextStyle
+
+class InputField(ft.UserControl):
+    def __init__(self, hint_text):
+        super().__init__()
+        self.body = TextField(
+            hint_text=hint_text,
+            width=250,
+            border_radius = 40,
+            bgcolor="#FFFFFF",
+            border_width=0.5,
+            border_color="#000000",
+            height=60, 
+            hint_style=TextStyle(color='black'),
+            text_style=TextStyle(size=14,weight='w400')
+        )
+  
+    def build(self):
+        return self.body
--- frontend/views.py
+++ frontend/views.py
@@ -1,29 +1,65 @@
 import flet as ft
+from flet import Text, Container, Column, Row, Stack, margin, alignment, TextField, ElevatedButton, Image, border
+
+
+from .components.inputField import InputField 
+from .layouts.form_layout import FormLayout
+
+bgColor = "#0CA6E9"
 
 def home(page: ft.Page, **kwargs) -> ft.View:
     # Create the view
     view = ft.View()
-
-    def send_name(event):
+    
+   # view.vertical_alignment=ft.MainAxisAlignment.CENTER
+    view.bgcolor= bgColor
+    def send_cadastro(event):
         page.go(f'/hello/{field.value}')
+        
+    body = FormLayout([
+        InputField('E-mail'),
+        InputField('Senha'),
+        ElevatedButton(text="Entrar",width=250,height=50.7,bgcolor="#00FFA3")
+    ], 'Primeira vez?', 'Registre-se', page, 'cadastro')
 
-    # Add elements
-    view.controls.append(ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT))
-
-    field = ft.TextField(label='Seu nome') # Get user input
-    view.controls.append(field)
-    view.controls.append(ft.ElevatedButton("Dizer oi", on_click=send_name))
-
+    view.controls.append(body)
+      ## view.controls.append(ft.ElevatedButton("Dizer oi", on_click=send_name))
+   
+    
     # Return the view
     return view
 
-def hello(page: ft.Page, name) -> ft.View:
+def hello(page: ft.Page) -> ft.View:
     # Create the view
+    page.padding = 0
+    page.window_resizable = false,
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
+    
     view = ft.View()
 
     # Add elements
-    view.controls.append(ft.Text(value=f'hello {name}'))
+   
 
     # Return the view
     return view
 
+##ft.app(name=agenda, target=home)
+
+def cadastro(page: ft.Page) -> ft.View:
+    
+    """ page.padding = 0
+    page.window_resizable = false,
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center' """
+    
+    view = ft.View()
+    view.bgcolor=bgColor
+    body = FormLayout([
+        InputField('Nome'),
+        InputField('E-mail'),
+        InputField('Senha'),
+        ElevatedButton(text="Criar conta",width=250,height=50.7,bgcolor="#00FFA3")
+    ], 'Já tem conta?', 'Clique aqui', page, '')
+    view.controls.append(body)
+    return view
