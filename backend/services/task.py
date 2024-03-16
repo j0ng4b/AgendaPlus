aa87a718e4baa8dbdd5d344c6c5abc0cc0602746
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-03-16 16:58:21 -0300

    added task service

---
+++ backend/services/task.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List, Optional
+
+from kink import inject
+
+from backend.models import Task
+from backend.unit_of_work.generic import IUnitOfWork
+
+
+class ITaskService(ABC):
+    _uow: IUnitOfWork
+
+    @abstractmethod
+    def add(self, task: Task) -> Task:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_date(self, user_id: int, date: datetime) -> List[Task]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_user_id(self, user_id: int) -> List[Task]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, task_id: int) -> Optional[Task]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all(self) -> List[Task]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, task: Task) -> Task:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, task_id: int) -> None:
+        raise NotImplementedError
+
+
+@inject
+class TaskService(ITaskService):
+    def __init__(self, uow: IUnitOfWork) -> None:
+        self._uow = uow
+
+    def add(self, task: Task) -> Task:
+        new_task = self._uow.task.add(task)
+        self._uow.save()
+
+        return new_task
+
+    def get_by_date(self, user_id: int, date: datetime) -> List[Task]:
+        return self._uow.task.get_by_date(user_id, date)
+
+    def get_by_user_id(self, user_id: int) -> List[Task]:
+        return self._uow.task.get_by_user_id(user_id)
+
+    def get_by_id(self, task_id: int) -> Optional[Task]:
+        return self._uow.task.get_by_id(task_id)
+
+    def get_all(self) -> List[Task]:
+        return self._uow.task.get_all()
+
+    def update(self, task: Task) -> Task:
+        updated_task = self._uow.task.update(task)
+        self._uow.save()
+
+        return updated_task
+
+    def delete(self, task_id: int) -> None:
+        if self._uow.task.get_by_id(task_id) is None:
+            return
+
+        self._uow.task.delete(task_id)
+        self._uow.save()
