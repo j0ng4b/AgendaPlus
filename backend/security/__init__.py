235181ac5e40ba16f6c9a88de864fbd96a5be08d
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-03-16 18:23:19 -0300

    updated authenticate function
    now it's set the user id to decorated function.

--- backend/security/__init__.py
+++ backend/security/__init__.py
@@ -1,41 +1,46 @@
 import functools
 from os import environ
-from typing import Any, Callable
+from typing import Any, Callable, Optional, cast
 
-from flask import Response, request
+from flask import request
 
 from backend.blueprints import BadAPIUsage, HTTPStatus
 from backend.security import jwt
 
 
-def authenticate(function: Callable[..., Any]) -> Callable[..., Any]:
-    @functools.wraps(function)
-    def wrapper(*args: list[Any], **kwargs: dict[str, Any]) -> Any:
-        header = request.headers.get('Authorization')
-        if header is None:
-            raise BadAPIUsage('no authorization token provided')
+def authenticate(pass_id: Optional[str] = None) -> Callable[..., Any]:
+    def inner(function: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(function)
+        def wrapper(*args: list[Any], **kwargs: dict[str, Any]) -> Any:
+            header = request.headers.get('Authorization')
+            if header is None:
+                raise BadAPIUsage('no authorization token provided')
 
-        if not header.startswith('Bearer'):
-            raise BadAPIUsage(
-                'invalid authorization method',
-                status_code=HTTPStatus.UNAUTHORIZED
-            )
+            if not header.startswith('Bearer '):
+                raise BadAPIUsage(
+                    'invalid authorization method',
+                    status_code=HTTPStatus.UNAUTHORIZED
+                )
 
-        try:
-            jwt.verify(
-                header[7:],
-                secret_key=str(environ.get('AUTH_AT_SECRET_KEY'))
-            )
-        except jwt.JWTInvalidToken:
-            raise BadAPIUsage(
-                'invalid access token',
-                status_code=HTTPStatus.UNAUTHORIZED
-            )
-        except jwt.JWTExpiredToken:
-            raise BadAPIUsage(
-                'expired access token',
-                status_code=HTTPStatus.UNAUTHORIZED
-            )
+            try:
+                payload = jwt.verify(
+                    header[7:],
+                    secret_key=str(environ.get('AUTH_AT_SECRET_KEY'))
+                )
+            except jwt.JWTInvalidToken:
+                raise BadAPIUsage(
+                    'invalid access token',
+                    status_code=HTTPStatus.UNAUTHORIZED
+                )
+            except jwt.JWTExpiredToken:
+                raise BadAPIUsage(
+                    'expired access token',
+                    status_code=HTTPStatus.UNAUTHORIZED
+                )
 
-        return function(*args, **kwargs)
-    return wrapper
+            if pass_id is not None:
+                kwargs[pass_id] = cast(Any, payload['id'])
+
+            return function(*args, **kwargs)
+        return wrapper
+    return inner
